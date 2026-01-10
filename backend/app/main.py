9c07e5d71def1d6432b417814fd05ca3fcf2d557
DebugWithPranjal <debugwithpranjal@gmail.com>
2026-01-10 20:33:36 +0530

    added requirements.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Sentry Lite")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
